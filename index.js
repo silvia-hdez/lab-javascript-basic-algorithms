3d77689ab789ab234636caef5cd517e39465eb0d
silvi-hdez <silviahdez.b@gmail.com>
2025-03-20 21:59:13 +0100

    Bonus 1 y 2 Done but they are not perfect. Check mistakes

--- index.js
+++ index.js
@@ -43,3 +43,35 @@ if (hacker1.localeCompare(hacker2) === -1) {
 } else {
   console.log("What?! You both have the same name?");
 }
+
+// Bonus 1
+
+let longText = "Lorem Ipsum is simply dummy text is of isthe printing and typesetting isindustry."
+let wordsNum = longText.length
+let wordSearch = "is"
+console.log(wordsNum)
+let counter = 0
+for(let i=0; i<wordsNum; i++){
+    if(longText.indexOf(wordSearch, i) != -1){
+        counter++
+        i=longText.indexOf(wordSearch, i)+wordSearch.length
+    }
+}
+console.log(counter)
+
+// Bonus 2
+
+let phraseToCheck = 'A man, a plan, a canal, Panama!'
+let phraseJoined = phraseToCheck.replace(/ /g,"")
+
+let reversephrase = ''
+
+for(let i=phraseJoined.length-1; i>=0; i--){
+  reversephrase += phraseJoined[i]
+}
+
+if(phraseJoined === reversephrase) {
+    console.log("It's a Palindrome!")
+} else {
+    console.log("It's not a Palindrome")
+}
